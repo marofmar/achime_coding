274373d9c60efeec28a55bbba2b3120d3f394d19
Yujin C. <yujin.mar.chung@gmail.com>
2024-11-29 09:39:27 +0900

    Update 242.dart

--- 242.dart
+++ 242.dart
@@ -11,3 +11,27 @@ class Solution {
     return sortedS.join() == sortedT.join();
   }
 }
+
+// 2. 해쉬 테이블로 풀기
+
+class Solution {
+    bool isAnagram(String s, String t) {
+        if (s.length != t.length) return false;
+
+        Map<String, int> counter = {};
+
+        for (int i = 0; i<s.length; i++) {
+            String char = s[i];
+            counter[char] = (counter[char] ?? 0) +1;
+        }
+
+        for (int j = 0; j<t.length; j++) {
+            String char = t[j];
+            if (!counter.containsKey(char) || counter[char] == 0) {
+                return false;
+            }
+            counter[char] = counter[char]!-1;
+        }
+        return true;
+    }
+}
