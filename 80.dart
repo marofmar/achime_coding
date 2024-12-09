817ee24ad46b376a3056039b9c4c9192fc6d558b
Yujin C. <yujin.mar.chung@gmail.com>
2024-12-09 09:28:48 +0900

    24-12-09

---
+++ 80.dart
@@ -0,0 +1,14 @@
+// remove duplicates 
+class Solution {
+  int removeDuplicates(List<int> nums) {
+    if (nums.length <=2) return nums.length;
+    int j = 2;
+    for (int i = 2; i<nums.length; i++) {
+        if (nums[i] != nums[j-2]) {
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+    return j;
+  }
+}
