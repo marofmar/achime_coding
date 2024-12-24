bf2afb1efc09dc293806b899945f7d8da63d606c
Yujin C. <yujin.mar.chung@gmail.com>
2024-12-24 09:57:31 +0900

    24-12-24

---
+++ 739.dart
@@ -0,0 +1,16 @@
+class Solution {
+  List<int> dailyTemperatures(List<int> temperatures) {
+    int n = temperatures.length;
+    List<int> result = List.filled(n, 0);
+    List<int> stack = [];
+
+    for (int i = 0; i<n; i++ ){
+        while (stack.isNotEmpty && temperatures[i]>temperatures[stack.last]) {
+            int prevIndex = stack.removeLast();
+            result[prevIndex] = i-prevIndex;
+        }
+        stack.add(i);
+    }
+    return result;
+  }
+}
